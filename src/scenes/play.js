2f77caadb728c71a0af0292ea33dc436cf39b104
ColinSORourke <csorourk@ucsc.edu>
2021-11-24 16:27:01 -0800

    Limited rotating

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -9,12 +9,14 @@ class Play extends Phaser.Scene {
         key3 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
         key4 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
         key5 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
-        keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC)
+        keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     }
 
     create(){
         this.currentlyRotating = false;
-        console.log("We did it!")
+        //console.log("We did it!")
 
         this.bgImage = this.add.rectangle(0, 0, game.config.width, game.config.height, 0xaaaaaa, 1).setOrigin(0,0);
 
@@ -36,25 +38,24 @@ class Play extends Phaser.Scene {
             this.pentagonCenterY,
             "pentagon cover",
         ).setAlpha(.8);
-        
-        if (!this.currentlyRotating){
-            this.input.keyboard.on("keydown-UP", () => {
-                this.rotatePentagonUp();
-            });
-            this.input.keyboard.on("keydown-DOWN", () => {
-                this.rotatePentagonDown();
-            });
-        }
-
     }
 
     update(){
-        
+        if (!this.currentlyRotating){
+            if ( Phaser.Input.Keyboard.JustUp(keyUP) ){
+                console.log(this.currentlyRotating);
+                this.rotatePentagonUp();
+            }
+            else if ( Phaser.Input.Keyboard.JustDown(keyDOWN) ){
+                console.log(this.currentlyRotating);
+                this.rotatePentagonDown();
+            }
+        }
     }
 
     rotatePentagonUp(){
         this.currentlyRotating = true;
-        console.log("rotate UP");
+        //console.log("rotate UP");
         this.pentagonRotationState--;
         if (this.pentagonRotationState == 0) {
             this.pentagonRotationState = 5;
@@ -68,8 +69,8 @@ class Play extends Phaser.Scene {
             angleTarget = angle - 72;
         }
 
-        console.log("rotation state:", this.pentagonRotationState);
-        console.log("angle is:", 72*(this.pentagonRotationState - 1));
+        //console.log("rotation state:", this.pentagonRotationState);
+        //console.log("angle is:", 72*(this.pentagonRotationState - 1));
         this.tweens.add({
             targets: this.pentagonContainer,
             angle: angleTarget,
@@ -77,14 +78,15 @@ class Play extends Phaser.Scene {
             onComplete: function(){
                 this.currentlyRotating = false;
                 console.log("Should be able to rotate again");
-            }
+            },
+            onCompleteScope: this
         });
         //this.pentagonContainer.angle = 72*(this.pentagonRotationState - 1);
      
     }
     rotatePentagonDown(){
         this.currentlyRotating = true;
-        console.log("rotate DOWN");
+        //console.log("rotate DOWN");
         this.pentagonRotationState++;
         if (this.pentagonRotationState == 6) {
             this.pentagonRotationState = 1;
@@ -97,8 +99,8 @@ class Play extends Phaser.Scene {
         if (angleTarget == -angle) {
             angleTarget = angle + 72;
         }
-        console.log("rotation state:", this.pentagonRotationState);
-        console.log("angle is:", angleTarget);
+        //console.log("rotation state:", this.pentagonRotationState);
+        //console.log("angle is:", angleTarget);
         this.tweens.add({
             targets: this.pentagonContainer,
             angle: angleTarget,
@@ -106,7 +108,8 @@ class Play extends Phaser.Scene {
             onComplete: function(){
                 this.currentlyRotating = false;
                 console.log("Should be able to rotate again");
-            }
+            },
+            onCompleteScope: this
         });
     }
 }
