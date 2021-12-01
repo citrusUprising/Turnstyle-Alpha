508e26bff51c6323530a6a1db3b16c06f3818f4d
Darcy Phipps <diphipps@ucsc.edu>
2021-11-30 16:57:03 -0800

    Fixed everything

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -420,7 +420,7 @@ class Play extends Phaser.Scene {
                 }
                 console.log(potentialOutputText)
                 this.time.delayedCall(
-                    1* index,
+                    500* index,
                     (outTextLines) => {this.textBoxText.text = outTextLines},
                     [potentialOutputText],
                     this
